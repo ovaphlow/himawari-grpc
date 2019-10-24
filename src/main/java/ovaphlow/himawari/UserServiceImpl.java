11fda5f599efa359f9b0a10ae093da778cc6cbdb
ovaphlow <ovaphlow@outlook.com>
2019-10-24 17:17:06 +0800

    ...

--- src/main/java/ovaphlow/himawari/UserServiceImpl.java
+++ src/main/java/ovaphlow/himawari/UserServiceImpl.java
@@ -5,6 +5,9 @@ import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,11 +21,20 @@ public class UserServiceImpl extends UserGrpc.UserImplBase {
         Gson gson = new Gson();
 
         try {
-            Map<String, Object> body = gson.fromJson(req.getData(), Map.class);
+            Connection conn = DBUtil.getConn();
+            String sql = "select u.id, u.dept_id, u.username, u.name, " +
+                    "(select super from himawari.auth where user_id = u.id) as super, " +
+                    "(select v from public.common where id = u.dept_id) as dept " +
+                    "from public.user as u " +
+                    "order by id desc limit 200";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
             Map<String, Object> map = new HashMap<>();
             map.put("message", "");
-            map.put("content", body.get("keyword").toString());
+            map.put("content", DBUtil.getList(rs));
             resp = gson.toJson(map);
+            conn.close();
         } catch (Exception e) {
             e.printStackTrace();
             Map<String, String> map = new HashMap<>();
@@ -40,18 +52,30 @@ public class UserServiceImpl extends UserGrpc.UserImplBase {
     public void save(UserRequest req, StreamObserver<UserReply> responseObserver) {
         String resp = "";
         Gson gson = new Gson();
-        logger.info("{}", req.getData());
-        logger.info("{}", gson.toJson(req.getData()));
+        Map<String, Object> map = new HashMap<>();
+        map.put("message", "");
+        map.put("content", "");
 
         try {
             Map<String, Object> body = gson.fromJson(req.getData(), Map.class);
-            Map<String, Object> map = new HashMap<>();
-            map.put("message", "");
-            map.put("content", body.get("keyword").toString());
+            logger.info("{}", body);
+            Double d = Double.parseDouble(body.get("dept_id").toString());
+            Connection conn = DBUtil.getConn();
+            String sql = "insert into public.user " +
+                    "(username, password, name, dept_id, remark) " +
+                    "values (?, ?, ?, ?, ?) " +
+                    "returning id";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, body.get("username").toString());
+            ps.setString(2, body.get("password").toString());
+            ps.setString(3, body.get("name").toString());
+            ps.setInt(4, d.intValue());
+            ps.setString(5, body.get("remark").toString());
+            ResultSet rs = ps.executeQuery();
+            map.put("content", DBUtil.getMap(rs));
             resp = gson.toJson(map);
         } catch (Exception e) {
             e.printStackTrace();
-            Map<String, String> map = new HashMap<>();
             map.put("message", "gRPC服务器错误");
             resp = gson.toJson(map);
         }
