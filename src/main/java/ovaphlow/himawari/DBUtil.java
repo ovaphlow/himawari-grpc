8a78d7275ab42681290dd976c79774a43337180b
ovaphlow <ovaphlow@outlook.com>
2020-01-19 08:39:53 +0800

    comment

--- src/main/java/ovaphlow/himawari/DBUtil.java
+++ src/main/java/ovaphlow/himawari/DBUtil.java
@@ -20,6 +20,7 @@ public class DBUtil {
         config.setJdbcUrl("jdbc:postgresql://" + Global.getDbUrl());
         config.setUsername(Global.getDbUsername());
         config.setPassword(Global.getDbPassword());
+//        空闲时维持的连接数（最小连接数），默认与最大连接数同值。
 //        config.setMinimumIdle(0);
         config.setMaximumPoolSize(Global.getDbPoolSize());
         config.addDataSourceProperty("cachePrepStmts", "true");
